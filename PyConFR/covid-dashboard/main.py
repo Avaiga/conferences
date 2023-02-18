722ce7e5696785fb80410b8dd37bfcb50f6c3fd4
FlorianJacta <florian.jacta@avaiga.com>
2023-02-18 10:51:44 +0100

    update

--- PyConFR/covid-dashboard/main.py
+++ PyConFR/covid-dashboard/main.py
@@ -6,7 +6,7 @@ from taipy.gui import Gui
 from pages.country import country_md, on_change_country, selected_representation, data_country_date, pie_chart
 from pages.world import world_md
 from pages.map import map_md
-from pages.predictions import predictions_md
+from pages.predictions_sol import predictions_md
 
 from data.data import data
 
